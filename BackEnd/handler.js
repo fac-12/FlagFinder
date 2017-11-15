f25f78114677256e701df407f831e737b3e7a7ad
parkhabit <parkhabit@gmail.com>
2017-11-15 14:55:26 +0000

    setup homeHandler and staticFileHandler functions in the backend

---
+++ BackEnd/handler.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var path = require('path');
+
+function homeHandler(request, response) {
+  var filePath = path.join(__dirname, '..', 'FrontEnd', 'index.html');
+  fs.readFile(filePath, function(err, file){
+    if(err) console.log(err);
+
+    response.writeHead(200, 'Content-Type: text/html');
+    response.end(file);
+  });
+}
+
+function staticFileHandler(request, response, url) {
+  const extensionType = {
+    html : 'text/html',
+    css : 'text/css',
+    js : 'application/javascript',
+    ico : 'image/x-icon',
+    jpg : 'image/jpeg'
+  }
+
+  var extension = url.split('.')[1];
+
+  var filePath = path.join(__dirname, '..', 'FrontEnd', url);
+
+  fs.readFile(filePath, function(err, file) {
+    if(err) console.log(err);
+    reponse.writeHead(200, 'Content-Type: ' + extensionType[extension]);
+    response.end(file);
+  });
+}
+
+function autocompleteHandler(request, response){
+
+}
+
+module.exports = {
+  homeHandler,
+  staticFileHandler,
+  autocompleteHandler
+}
