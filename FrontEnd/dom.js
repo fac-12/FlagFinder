c596bfc25963cb0b19e97c6d2cd1e8914aa8339f
sophielevens <sophielevens1@gmail.com>
2017-11-15 18:07:35 +0000

    autocomplete function

--- FrontEnd/dom.js
+++ FrontEnd/dom.js
@@ -1,40 +1,57 @@
 const searchForm = document.getElementById('searchForm');
 
+
 let searchValue;
 
 searchForm.addEventListener('input', function(event) {
   searchValue = event.target.value;
+  let url = 'http://localhost:8000/autocomplete';
   var xhr = new XMLHttpRequest();
-    var obj;
+  var obj;
 
-    xhr.onreadystatechange = function(){
-      if(xhr.readyState == 4){
+  autocomplete([
+    "england",
+    "estonia",
+    "ecuador",
+    "egypt",
+    "emmental"
+  ]);
 
-        obj = JSON.parse(xhr.responseText);
-        callback(obj);
-      }
-    };
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4){
 
-    xhr.open("GET", url, true);
-    xhr.send();
+      obj = JSON.parse(xhr.responseText);
+
+    }
+  };
+
+  xhr.open("POST", url, true);
+  xhr.send(searchValue);
 
 })
 
+function autocomplete(obj){
+  obj.forEach(function(country, index){
+    let option = document.getElementById(index.toString());
+    option.setAttribute("value", country);
+  })
+}
 
-searchForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  var xhr = new XMLHttpRequest();
-    var obj;
 
-    xhr.onreadystatechange = function(){
-      if(xhr.readyState == 4){
-
-        obj = JSON.parse(xhr.responseText);
-        callback(obj);
-      }
-    };
-
-    xhr.open("GET", url, true);
-    xhr.send();
-
-})
+// searchForm.addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   var xhr = new XMLHttpRequest();
+//     var obj;
+//
+//     xhr.onreadystatechange = function(){
+//       if(xhr.readyState == 4){
+//
+//         obj = JSON.parse(xhr.responseText);
+//         callback(obj);
+//       }
+//     };
+//
+//     xhr.open("GET", url, true);
+//     xhr.send();
+//
+// })
