a4dcf4e394c2a2da131e89191da8dd79653ba0f7
dragomir <svedava@mail.ru>
2017-11-16 18:41:08 +0000

    folders small letters

---
+++ backend/router.js
@@ -0,0 +1,25 @@
+var { homeHandler, staticFileHandler, autocompleteHandler} = require('./handler');
+
+function router(request, response){
+  var url = request.url;
+
+  if(url.indexOf('frontend') !== -1) {
+    staticFileHandler(request, response, url)
+    return;
+  }
+
+  switch (url) {
+    case '/':
+      homeHandler(request, response);
+      break;
+    case '/autocomplete':
+      autocompleteHandler(request, response);
+      break;
+    default:
+      response.writeHead(404);
+      response.end('404, File not found.');
+      return;
+  }
+}
+
+module.exports = router;
